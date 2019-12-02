96562c2dd72174a43b3321b68a4d5e39def5490f
sxVova <53950314+sxVova@users.noreply.github.com>
2019-12-02 12:38:04 +0200

    Add tf file

---
+++ main.tf
@@ -0,0 +1,38 @@
+resource "google_compute_instance" "web-server" {
+  name         = "deployment-webserver"
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Hello from Terraform on Google Cloud, Master!</h1></body></html>' | sudo tee /var/www/html/index.html && sudo chmod 666 index.html"
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+  // Apply the firewall rule to allow external IPs to access this instance
+  tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
